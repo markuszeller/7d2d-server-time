748811f9b3893a3c4567608bd3c850bc8faf257b
Markus Zeller <git@markuszeller.com>
2022-08-07 17:30:48 +0200

    Add try/catch

--- app/index.php
+++ app/index.php
@@ -5,6 +5,11 @@ use markuszeller\server\Server;
 require_once './src/Server.class.php';
 
 $server = new Server('yourserver.com');
-echo $server->fetch()
-    ->parse()
-    ->toHtml();
+
+try {
+    echo $server->fetch()
+        ->parse()
+        ->toHtml();
+} catch (\Exception $exception) {
+    echo "<h1>Error</h1><pre>{$exception->getMessage()}</pre>";
+}
